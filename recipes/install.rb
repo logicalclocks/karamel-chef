ac4a7ab06d595afabdad5fcb50483a4a64b84459
Jim Dowling <jdowling@sics.se>
2017-03-09 10:23:29 +0100

    centos support

--- recipes/install.rb
+++ recipes/install.rb
@@ -6,11 +6,38 @@ node.default.java.oracle.accept_oracle_download_terms = true
 
 include_recipe "java"
 
+case node['platform']
+when 'debian', 'ubuntu'
+  node[:karamel][:default][:private_ips].each_with_index do |ip, index| 
+    hostsfile_entry "#{ip}" do
+      hostname  "dn#{index}"
+      action    :create
+      unique    true
+    end
+  end
 
-node[:karamel][:default][:private_ips].each_with_index do |ip, index| 
-   hostsfile_entry "#{ip}" do
-     hostname  "dn#{index}"
-     action    :create
-     unique    true
-   end
+when 'redhat', 'centos', 'fedora'
+
+  template "/etc/hosts" do
+    source "hosts.erb"
+    owner "root"
+    group "root"
+    mode 0644
+  end
+
+# Fix bug: https://github.com/mitchellh/vagrant/issues/8115    
+bash "restart_network" do
+    user "root"
+    code <<-EOF
+  /etc/init.d/network restart  
+EOF
 end
+
+
+  
+end
+
+
+
+
+  
--- run.sh
+++ run.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+
+if [ $# -ne 1 ] ; then
+  echo "Usage: ./run.sh [centos|ubuntu]"
+  echo ""
+  exit 1
+fi
+
+if [ "$1" == "centos" ] ; then
+ cp Vagrantfile.centos Vagrantfile
+elif [ "$1" == "ubuntu" ] ; then
+ cp Vagrantfile.ubuntu Vagrantfile
+else
+  echo "Usage: ./run.sh [centos|ubuntu]"
+  echo ""
+  exit 1
+fi
+
 set -e
 echo "Removing old vendored cookbooks"
 rm -rf cookbooks > /dev/null 2>&1
