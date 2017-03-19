1b696c0111096b5773d95286b68e59cf0f40083b
Jim Dowling <jdowling@sics.se>
2017-03-19 17:45:56 +0100

    changed command switch in karamel

--- kill.sh
+++ kill.sh
@@ -4,5 +4,8 @@ pkill VBoxHeadless
 
 sleep 5
 pkill VBoxSVC
+#rm -rf ~/VirtualBox\ VMs/dn0
+#rm -rf ~/VirtualBox\ VMs/dn1
+#rm -rf ~/VirtualBox\ VMs/dn2
 
 vagrant destroy -f
--- recipes/run.rb
+++ recipes/run.rb
@@ -2,7 +2,7 @@ bash "run_karamel" do
     user "vagrant"
     code <<-EOF
 cd /home/vagrant/karamel-0.3
-./bin/karamel -headless -launch /home/vagrant/cluster.yml -nosudopasswd
+./bin/karamel -headless -launch /home/vagrant/cluster.yml 
 
 EOF
 end
