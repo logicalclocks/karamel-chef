53f9ab91a4c7f88ad8a13009c0e13c1ea41f32e1
Jim Dowling <jdowling@sics.se>
2017-08-10 00:41:08 +0200

    updates

--- recipes/run.rb
+++ recipes/run.rb
@@ -2,8 +2,25 @@ bash "run_karamel" do
     user "vagrant"
     timeout 36000
     code <<-EOF
-cd /home/vagrant/karamel-0.3
-./bin/karamel -headless -launch /home/vagrant/cluster.yml 
+    set -e
+    cd /home/vagrant/karamel-0.3
+    ./bin/karamel -headless -launch /home/vagrant/cluster.yml 
+EOF
+end
+
+
+package "git"
+
+package "maven"
+
+bash "hopsworks_tests" do
+    user "root"
+    timeout 36000
+    code <<-EOF
+    set -e
+    cd /home/vagrant
+    git clone git@github.com:hopshadoop/hopsworks.git
+        
 
 EOF
 end
