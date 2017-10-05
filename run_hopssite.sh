453fdb97b01163cf91ca25491d1610f77b379b78
Alex Ormenisan <ormenisan.adrian@gmail.com>
2017-10-05 15:19:41 +0200

    run hopssite and dela

--- run_hopssite.sh
+++ run_hopssite.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+./run.sh hopssite 1 hopssite no-random-ports
 
 echo "this is a hack fix for udp port forwarding - waiting for 1min - be patient"
 sleep 1m
 echo "abc" | nc -u bbc1.sics.se 42100
 echo "abc" | nc -u bbc1.sics.se 43100
 echo "abc" | nc -u bbc1.sics.se 44100
+echo "hack completed"
