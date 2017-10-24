6aaea313de66c9a5ed8e664202dd3a8c79b260f4
Alex Ormenisan <ormenisan.adrian@gmail.com>
2017-10-24 14:41:38 +0200

    defaults fix

--- udp_hacky_fix.sh
+++ udp_hacky_fix.sh
@@ -3,7 +3,7 @@
 echo "this is a hack fix for udp port forwarding - waiting for 1min - be patient"
 echo "if nc prints a warning/error - the dela service might not work on your installation"
 sleep 1m
-echo "abc" | nc -u localhost 42001
-echo "abc" | nc -u localhost 42002
-echo "abc" | nc -u localhost 42003
+echo "abc" | nc -u localhost 42011
+echo "abc" | nc -u localhost 42012
+echo "abc" | nc -u localhost 42013
 echo "hack completed"
