6efa391c190f84ac6e84f6a77f45efef3a73b22b
Alex Ormenisan <ormenisan.adrian@gmail.com>
2017-12-15 16:02:33 +0100

    array loop

--- scripts/dela_setup.sh
+++ scripts/dela_setup.sh
@@ -11,7 +11,7 @@ materialize()
   echo "$FILE materializing"
   rm -f $FILE
   cp $TEMPLATE $FILE
-  for val in $VALUES
+  for val in ${VALUES[@]}
   do 
     echo "$val = ${!val}"
     sed -i -e "s/{$val}/${!val}/g" $FILE
@@ -34,13 +34,13 @@ if [ $CLUSTER_MULTI_USER == true ] ; then
 else
   USER_SETTING="user: vagrant"
 fi
-VALUES=("OS_VERSION" "SSH_P" "MYSQL_P" "KARAMEL_P" "WEB_P" "DEBUG_P" "GFISH_P" "DELA1_P" "DELA2_P" "DELA3_P" "DELA4_P" "PORT1" "PORT2" "PORT3" "PORT4" "PORT5" "PORT6" "PORT7" "PORT8" "PORT9")
+declare -a VALUES=("OS_VERSION" "SSH_P" "MYSQL_P" "KARAMEL_P" "WEB_P" "DEBUG_P" "GFISH_P" "DELA1_P" "DELA2_P" "DELA3_P" "DELA4_P" "PORT1" "PORT2" "PORT3" "PORT4" "PORT5" "PORT6" "PORT7" "PORT8" "PORT9")
 materialize "../vagrantfiles/Vagrantfile.dela.1" "../vagrantfiles/Vagrantfile.dela_template.1" $VALUES
-VALUES=("GITHUB" "BRANCH" "NETWORK_INTERFACE" "USER_SETTING" "WEB_P" "DELA1_P" "DELA2_P" "DELA3_P" "DELA4_P" "HS_WEB1_P" "HS_WEB2_P" "CLUSTER_MANUAL_REGISTER" "HOPSSITE_DOMAIN" "CLUSTER_EMAIL" "SOURCE_CODE" "CLUSTER_ORG" "CLUSTER_UNIT" "HOPSSITE_PASSWORD")
+declare -a VALUES=("GITHUB" "BRANCH" "NETWORK_INTERFACE" "USER_SETTING" "WEB_P" "DELA1_P" "DELA2_P" "DELA3_P" "DELA4_P" "HS_WEB1_P" "HS_WEB2_P" "CLUSTER_MANUAL_REGISTER" "HOPSSITE_DOMAIN" "CLUSTER_EMAIL" "SOURCE_CODE" "CLUSTER_ORG" "CLUSTER_UNIT" "HOPSSITE_PASSWORD")
 materialize "../cluster-defns/1.dela.yml" "../cluster-defns/1.dela_template.yml" $VALUES
-VALUES=("CLUSTER_EMAIL" "CLUSTER_PASSWORD" "CLUSTER_ORG" "CLUSTER_UNIT" "HOPSSITE_DOMAIN" "HS_WEB1_P")
+declare -a VALUES=("CLUSTER_EMAIL" "CLUSTER_PASSWORD" "CLUSTER_ORG" "CLUSTER_UNIT" "HOPSSITE_DOMAIN" "HS_WEB1_P")
 materialize "dela_register.sh" "dela_register_template.sh" $VALUES
 chmod +x dela_register.sh
-VALUES=("CLUSTER_DOMAIN" "DELA1_P" "DELA2_P" "DELA3_P")
+declare -a VALUES=("CLUSTER_DOMAIN" "DELA1_P" "DELA2_P" "DELA3_P")
 materialize "udp_hacky_fix.sh" "udp_hacky_fix_template.sh" $VALUES
 chmod +x udp_hacky_fix.sh
