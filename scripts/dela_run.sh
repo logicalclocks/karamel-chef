7e2dc45fd85d4a572261c254d60d1c02a36a8a50
Alex Ormenisan <ormenisan.adrian@gmail.com>
2017-12-15 17:19:05 +0100

    fixes

--- scripts/dela_run.sh
+++ scripts/dela_run.sh
@@ -4,7 +4,7 @@ if [ ! -d "scripts" ]; then
   echo "Run the script from the karamel-chef dir"
   exit 1
 fi
-KCHEF_DIR=${pwd}
+KCHEF_DIR=${PWD}
 ${KCHEF_DIR}/scripts/dela_setup.sh
 ${KCHEF_DIR}/run.sh dela 1 dela no-random-ports
 ${KCHEF_DIR}/scripts/running/udp_hacky_fix.sh
