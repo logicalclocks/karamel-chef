96acd8f805274dcc48638050291017765d94db1f
Alex Ormenisan <ormenisan.adrian@gmail.com>
2017-12-18 08:54:06 +0100

    fix

--- dela/templates/register_template.sh
+++ dela/templates/register_template.sh
@@ -3,4 +3,4 @@ set -e
 cd dela/running
 CONTENT_TYPE="Content-Type: application/json"
 TARGET=http://{HOPSSITE_DOMAIN}:{HS_WEB1_P}/hopsworks-cluster/api/cluster/register
-curl -d "@register_data.json" -H $CONTENT_TYPE -X POST $TARGET
+curl -d "@register_data.json" -H "$CONTENT_TYPE" -X POST $TARGET
