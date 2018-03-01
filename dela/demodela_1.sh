f23b21738990f16165aace8c18b319d6f3c32477
Alex Ormenisan <ormenisan.adrian@gmail.com>
2018-03-01 16:39:46 +0100

    fixes

--- dela/demodela_1.sh
+++ dela/demodela_1.sh
@@ -5,14 +5,14 @@ if [ ! -d "dela" ]; then
 fi
 KCHEF_DIR=${PWD}
 rm cluster-defns/1.demodela.yml
-cp templates/1.demodela.yml cluster-defns/
+cp dela/templates/1.demodela.yml cluster-defns/
 
 #random 32 character alphanumeric string (upper and lowercase)
 ID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 #3base + random 7 character alphanumeric string (upper and lowercase)
 PASS="1Ab"+$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 7 | head -n 1)
 EMAIL="demo"+${ID}+"@hops.io"
-sed -i -e "s/register_email/${EMAIL}/g" $FILE
-sed -i -e "s/register_organization/demohops/g" $FILE
-sed -i -e "s/register_organization_unit/${ID}/g" $FILE
-sed -i -e "s/register_pass/${PASS}/g" $FILE
+sed -i -e "s/REGISTER_EMAIL/${EMAIL}/g" $FILE
+sed -i -e "s/REGISTER_ORGANIZATION/demohops/g" $FILE
+sed -i -e "s/REGISTER_ORGANIZATION_UNIT/${ID}/g" $FILE
+sed -i -e "s/REGISTER_PASSWORD/${PASS}/g" $FILE
