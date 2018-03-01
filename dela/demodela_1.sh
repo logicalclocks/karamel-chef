2e47e0cb3a1ed565472c9528c173fdde472714c2
Alex Ormenisan <ormenisan.adrian@gmail.com>
2018-03-01 17:02:41 +0100

    fixes

--- dela/demodela_1.sh
+++ dela/demodela_1.sh
@@ -10,10 +10,10 @@ cp dela/templates/1.demodela.yml cluster-defns/
 #random 32 character alphanumeric string (upper and lowercase)
 ID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 #3base + random 7 character alphanumeric string (upper and lowercase)
-PASS="1Ab"+$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 7 | head -n 1)
-EMAIL="demo"+${ID}+"@hops.io"
+PASS="1Ab$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 7 | head -n 1)"
+EMAIL="demo_${ID}@hops.io"
 FILE=cluster-defns/1.demodela.yml
 sed -i -e "s/REGISTER_EMAIL/${EMAIL}/g" $FILE
 sed -i -e "s/REGISTER_ORGANIZATION/demohops/g" $FILE
-sed -i -e "s/REGISTER_ORGANIZATION_UNIT/${ID}/g" $FILE
+sed -i -e "s/REGISTER_ORG_UNIT/${ID}/g" $FILE
 sed -i -e "s/REGISTER_PASSWORD/${PASS}/g" $FILE
