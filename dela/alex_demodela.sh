eabf6e8ca0af9e30f6c6acf1dd9bfa8d1d9468a3
Alex Ormenisan <ormenisan.adrian@gmail.com>
2018-03-07 09:44:46 +0100

    scripts for vm image

--- dela/alex_demodela.sh
+++ dela/alex_demodela.sh
@@ -4,5 +4,5 @@ if [ ! -d "dela" ]; then
   echo "Run the script from the karamel-chef dir"
   exit 1
 fi
-./dela/alex_demodela_1.sh alex_demodela
+./dela/demodela_1.sh alex_demodela
 ./run.sh ubuntu 1 demodela
---
+++ dela/vm_image_scripts/copy_scripts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+if [ $# -ne 1 ] ; then
+  echo "first param - vm ssh forwarder port"
+  exit 1
+fi
+if [ ! -d "dela" ]; then
+  echo "Run the script from the karamel-chef dir"
+  exit 1
+fi
+SSH_PORT=$1
+scp -i ~/.vagrant.d/insecure_private_key -P ${SSH_PORT} dela/vm_image/scripts/csr-ca.py vagrant@localhost:/srv/hops/domains/domain1/bin
+ssh -i ~/.vagrant.d/insecure_private_key -p ${SSH_PORT} vagrant@localhost 'mkdir -p /srv/hops/hopssite'
+scp -i ~/.vagrant.d/insecure_private_key -P ${SSH_PORT} dela/vm_image/scripts/image_register.sh vagrant@localhost:/srv/hops/hopssite
+scp -i ~/.vagrant.d/insecure_private_key -P ${SSH_PORT} dela/vm_image/scripts/register_dela_template.json vagrant@localhost:/srv/hops/hopssite
