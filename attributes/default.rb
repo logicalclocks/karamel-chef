fc64189be3658b0cbd4380a59e818f5f9f5e301d
Alex Ormenisan <ormenisan.adrian@gmail.com>
2018-07-18 15:21:34 +0200

    Fix for failing java recipe. Set your own java build through the java attributes. The default oracle build used by the java recipe(172) has been archived

--- attributes/default.rb
+++ attributes/default.rb
@@ -21,3 +21,9 @@ default['test']['hopsworks']['test_dir']    = "#{node['test']['hopsworks']['base
 default['test']['hopsworks']['report_dir']      = "/home/vagrant/test_report"
 
 default['test']['anaconda_cache']['version'] = "5.0.1"
+
+default['java']['jdk_version'] = '8'
+default['java']['install_flavor'] = 'oracle'
+default['java']['jdk']['8']['x86_64']['url'] = 'http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jdk-8u181-linux-x64.tar.gz'
+default['java']['jdk']['8']['x86_64']['checksum'] = '1845567095bfbfebd42ed0d09397939796d05456290fb20a83c476ba09f991d3'
+default['java']['oracle']['accept_oracle_download_terms'] = true
