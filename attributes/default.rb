36169f594e85d1e6a6f9099de3d3a4fff70579a7
Fabio Buso <buso.fabio@gmail.com>
2018-09-12 11:35:44 +0200

    Do not copy Anaconda.sh during the tests

--- attributes/default.rb
+++ attributes/default.rb
@@ -20,8 +20,6 @@ default['test']['hopsworks']['base_dir']    = "/home/vagrant/hopsworks"
 default['test']['hopsworks']['test_dir']    = "#{node['test']['hopsworks']['base_dir']}/hopsworks-ear/test"
 default['test']['hopsworks']['report_dir']      = "/home/vagrant/test_report"
 
-default['test']['anaconda_cache']['version'] = "5.0.1"
-
 default['java']['jdk_version'] = '8'
 default['java']['install_flavor'] = 'oracle'
 default['java']['jdk']['8']['x86_64']['url'] = 'http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jdk-8u181-linux-x64.tar.gz'
