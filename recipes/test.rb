31564f1a312289ca1afa0a86f54c13fb2c0dbb90
Fabio Buso <buso.fabio@gmail.com>
2018-12-13 23:01:06 +0100

    Update ruby installation for centos test vm

--- recipes/test.rb
+++ recipes/test.rb
@@ -10,8 +10,8 @@ when 'centos'
     group "root"
     code <<-EOH
       yum install gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison iconv-devel sqlite-devel
-      curl -sSL https://rvm.io/mpapis.asc | gpg --import -
-      curl -L get.rvm.io | bash -s stable
+      gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+      curl -sSL https://get.rvm.io | bash -s stable
       source /etc/profile.d/rvm.sh
       rvm reload
       rvm install 2.4.1
