b464db68a3b2135d1cad93f24d0ef1c09d1d4b04
Fabio Buso <buso.fabio@gmail.com>
2018-12-19 11:40:54 +0100

    Remove source rvm and explicitally export envs

--- recipes/test.rb
+++ recipes/test.rb
@@ -69,11 +69,13 @@ when 'centos'
     timeout node['karamel']['test_timeout']
     cwd node['test']['hopsworks']['test_dir']
     environment ({'PATH' => "#{ENV['PATH']}:/usr/local/bin:/srv/hops/mysql/bin",
-                  'LD_LIBRARY_PATH' => "#{ENV['LD_LIBRARY_PATH']}:/srv/hops/mysql/lib",
-                  'JAVA_HOME' => "/usr/lib/jvm/java"})
+              'LD_LIBRARY_PATH' => "#{ENV['LD_LIBRARY_PATH']}:/srv/hops/mysql/lib",
+              'HOME' => "/home/vagrant",
+              'GEM_PATH' => "/usr/local/rvm/gems/ruby-2.4.1:/usr/local/rvm/gems/ruby-2.4.1@global",
+              'GEM_HOME' => "/usr/local/rvm/gems/ruby-2.4.1",
+              'JAVA_HOME' => "/usr/lib/jvm/java"})
     code <<-EOH
       set -e
-      source /usr/local/rvm/scripts/rvm
       rvm use 2.4.1
       gem install bundler
       bundle install
