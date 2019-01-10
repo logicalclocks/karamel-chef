dbb36135715c42d781a23e9288ff2586fa225e9b
Fabio Buso <buso.fabio@gmail.com>
2019-01-10 14:51:41 +0100

    [HOPSWORKS-867] Move from Oracle JDK to OpenJDK from the package manager

--- attributes/default.rb
+++ attributes/default.rb
@@ -22,7 +22,4 @@ default['test']['hopsworks']['test_dir']    = "#{node['test']['hopsworks']['base
 default['test']['hopsworks']['report_dir']      = "/home/vagrant/test_report"
 
 default['java']['jdk_version'] = '8'
-default['java']['install_flavor'] = 'oracle'
-default['java']['jdk']['8']['x86_64']['url'] = 'http://download.oracle.com/otn-pub/java/jdk/8u192-b12/750e1c8617c5452694857ad95c3ee230/jdk-8u192-linux-x64.tar.gz'
-default['java']['jdk']['8']['x86_64']['checksum'] = '6d34ae147fc5564c07b913b467de1411c795e290356538f22502f28b76a323c2'
-default['java']['oracle']['accept_oracle_download_terms'] = true
+default['java']['install_flavor'] = 'openjdk'
