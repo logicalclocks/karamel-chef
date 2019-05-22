fd9eba674e6310e821ab57e4be9827566af009d1
Kim Hammar <kimham@kth.se>
2019-05-22 14:00:56 +0200

    [HOPSWORKS-751] add support for SkLearn model serving
    - add integration test for serving API in hops-util-py

--- recipes/it_tests.rb
+++ recipes/it_tests.rb
@@ -13,6 +13,13 @@ hopsUtilpyTestData5Name = "teams_features.csv"
 hopsUtilpyTestData5Url = "#{hopsUtilpyTestArtifactsDirUrl}/#{hopsUtilpyTestData5Name}"
 hopsUtilpyTestDataNotebookName = "integration_tests.ipynb"
 hopsUtilpyTestDataNotebookUrl = "#{hopsUtilpyTestArtifactsDirUrl}/#{hopsUtilpyTestDataNotebookName}"
+hopsUtilpyTestDataSkLearnModelName = "iris_knn.pkl"
+hopsUtilpyTestDataSkLearnModelUrl = "#{hopsUtilpyTestArtifactsDirUrl}/#{hopsUtilpyTestDataSkLearnModelName}"
+hopsUtilpyTestDataSkLearnModelScriptName = "iris_flower_classifier.py"
+hopsUtilpyTestDataSkLearnModelScriptUrl = "#{hopsUtilpyTestArtifactsDirUrl}/#{hopsUtilpyTestDataSkLearnModelScriptName}"
+hopsUtilpyTestDataTfModelZipName = "mnist.tar.gz"
+hopsUtilpyTestDataTfModelUrl = "#{hopsUtilpyTestArtifactsDirUrl}/#{hopsUtilpyTestDataTfModelZipName}"
+hopsUtilpyTestDataTfModelDir = "mnist"
 
 
 hops_hdfs_directory "#{hopsUtilpyTestArtifactsDirHdfs}" do
@@ -116,4 +123,69 @@ hops_hdfs_directory "#{Chef::Config['file_cache_path']}/#{File.basename(hopsUtil
   group 'hadoop'
   mode "1755"
   dest "/user/it_tests/#{hopsUtilpyTestDataNotebookName}"
+end
+
+remote_file "#{Chef::Config['file_cache_path']}/#{File.basename(hopsUtilpyTestDataSkLearnModelName)}" do
+  source hopsUtilpyTestDataSkLearnModelUrl
+  owner 'hdfs'
+  group 'hadoop'
+  mode "1775"
+  action :create
+end
+
+hops_hdfs_directory "#{Chef::Config['file_cache_path']}/#{File.basename(hopsUtilpyTestDataSkLearnModelName)}" do
+  action :replace_as_superuser
+  owner 'hdfs'
+  group 'hadoop'
+  mode "1755"
+  dest "/user/it_tests/#{hopsUtilpyTestDataSkLearnModelName}"
+end
+
+remote_file "#{Chef::Config['file_cache_path']}/#{File.basename(hopsUtilpyTestDataSkLearnModelScriptName)}" do
+  source hopsUtilpyTestDataSkLearnModelScriptUrl
+  owner 'hdfs'
+  group 'hadoop'
+  mode "1775"
+  action :create
+end
+
+hops_hdfs_directory "#{Chef::Config['file_cache_path']}/#{File.basename(hopsUtilpyTestDataSkLearnModelScriptName)}" do
+  action :replace_as_superuser
+  owner 'hdfs'
+  group 'hadoop'
+  mode "1755"
+  dest "/user/it_tests/#{hopsUtilpyTestDataSkLearnModelScriptName}"
+end
+
+remote_file "#{Chef::Config['file_cache_path']}/#{File.basename(hopsUtilpyTestDataTfModelZipName)}" do
+  source hopsUtilpyTestDataTfModelUrl
+  mode 0755
+  action :create
+end
+
+# Extract Model Zip
+bash 'extract_mnist_model' do
+  user "root"
+  code <<-EOH
+                set -e
+                cd #{Chef::Config['file_cache_path']}
+                rm -rf #{hopsUtilpyTestDataTfModelDir}
+                tar -zxf #{hopsUtilpyTestDataTfModelZipName}
+                chown -RL hdfs:hadoop #{Chef::Config['file_cache_path']}/#{hopsUtilpyTestDataTfModelDir}
+  EOH
+end
+
+hops_hdfs_directory "/user/it_tests/#{hopsUtilpyTestDataTfModelDir}" do
+  action :create_as_superuser
+  owner node['hops']['hdfs']['user']
+  group node['hops']['group']
+  mode "1775"
+end
+
+hops_hdfs_directory "#{Chef::Config['file_cache_path']}/#{hopsUtilpyTestDataTfModelDir}" do
+  action :replace_as_superuser
+  owner 'hdfs'
+  group 'hadoop'
+  mode "1755"
+  dest "/user/it_tests/#{hopsUtilpyTestDataTfModelDir}"
 end
