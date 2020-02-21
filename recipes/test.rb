725f6556a3419b2f0cc5a665ebc26f2340b9e313
Theofilos Kakantousis <t.kakantousis@gmail.com>
2020-02-21 19:31:42 +0100

    remove install from ubuntu

--- recipes/test.rb
+++ recipes/test.rb
@@ -7,7 +7,6 @@ when 'ubuntu'
     group "root"
     code <<-EOH
       # https://linuxize.com/post/how-to-install-ruby-on-ubuntu-18-04/
-      apt install curl g++, gcc, autoconf, automake, bison, libc6-dev, libffi-dev, libgdbm-dev, libncurses5-dev, libsqlite3-dev, libtool, libyaml-dev, make, pkg-config, sqlite3, zlib1g-dev, libgmp-dev, libreadline-dev, libssl-dev
       gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
       curl -sSL https://get.rvm.io | bash -s stable
       source /etc/profile.d/rvm.sh
