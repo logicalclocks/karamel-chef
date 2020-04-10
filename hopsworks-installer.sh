9da8559e0a3a0d47e4a84c9288e8cc729833d051
Jim Dowling <jdowling@sics.se>
2020-04-10 08:29:30 +0200

    fix cluster defn branch (#117)
    * improve error msg

--- hopsworks-installer.sh
+++ hopsworks-installer.sh
@@ -29,7 +29,7 @@
 
 HOPSWORKS_REPO=logicalclocks/hopsworks-chef
 HOPSWORKS_BRANCH=master
-CLUSTER_DEFINITION_BRANCH=https://raw.githubusercontent.com/logicalclocks/karamel-chef/azure_improvements
+CLUSTER_DEFINITION_BRANCH=https://raw.githubusercontent.com/logicalclocks/karamel-chef/master
 KARAMEL_VERSION=0.6
 INSTALL_ACTION=
 NON_INTERACT=0
