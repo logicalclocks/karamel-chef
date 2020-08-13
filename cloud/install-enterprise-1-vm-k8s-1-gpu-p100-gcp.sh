7d77586a67c1f54a6e84b4c96ce5743a2c05e7b2
Jim Dowling <jdowling@sics.se>
2020-08-13 16:24:52 +0200

    rename script

--- cloud/install-enterprise-1-vm-k8s-1-gpu-p100-gcp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-printf "Enter the Enterprise password: "
-read -s PASSWORD
-ENTERPRISE_PASSWORD=$PASSWORD ./hopsworks-cloud-installer.sh -n jr -i kubernetes -ni -c gcp -d https://nexus.hops.works/repository -du jim -w 0 -g 0 -gt p100 -gpus 1
---
+++ cloud/install-enterprise-1-vm-k8s-gcp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+printf "Enter the Enterprise username: "
+read -s USERNAME
+printf "Enter the Enterprise password: "
+read -s PASSWORD
+export ENTERPRISE_USERNAME=$USERNAME
+ENTERPRISE_PASSWORD=$PASSWORD ./hopsworks-cloud-installer.sh -n jr -i kubernetes -ni -c gcp -d https://nexus.hops.works/repository -w 0 -g 0
+# -gt p100 -gpus 1
