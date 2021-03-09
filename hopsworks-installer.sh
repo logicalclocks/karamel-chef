0fab04596ff205801f70a7ff555ee5d30ac5d329
Jim Dowling <jim@logicalclocks.com>
2021-03-09 08:42:23 +0100

    deploy 2.1

--- hopsworks-installer.sh
+++ hopsworks-installer.sh
@@ -28,7 +28,7 @@
 ###################################################################################################
 
 HOPSWORKS_REPO=logicalclocks/hopsworks-chef
-HOPSWORKS_BRANCH=master
+HOPSWORKS_BRANCH=2.1
 CLUSTER_DEFINITION_BRANCH=https://raw.githubusercontent.com/logicalclocks/karamel-chef/$HOPSWORKS_BRANCH
 KARAMEL_VERSION=0.6
 ENTERPRISE_DOWNLOAD_URL=https://nexus.hops.works/repository
