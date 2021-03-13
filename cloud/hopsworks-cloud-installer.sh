83ed3dcb2207e505b9e86e84f58fd242b44724a7
Jim Dowling <jim@logicalclocks.com>
2021-03-13 07:19:08 +0100

    change order of args when cleaning ssh keys

--- cloud/hopsworks-cloud-installer.sh
+++ cloud/hopsworks-cloud-installer.sh
@@ -443,8 +443,8 @@ cpus_gpus()
 
 clear_known_hosts()
 {
-    echo "ssh-keygen -f "~/.ssh/known_hosts" -R $host_ip"
-    ssh-keygen -f ~/.ssh/known_hosts -R $host_ip
+    echo "ssh-keygen -R $host_ip -f \"~/.ssh/known_hosts\""
+    ssh-keygen -R $host_ip -f ~/.ssh/known_hosts
     if [ $? -ne 0 ] ; then
 	echo ""	
 	echo "WARN: Could not clean up known_hosts file"
