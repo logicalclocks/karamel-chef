41b62be3f5d4fb1addccf564ad081dec9289d918
Jim Dowling <jim@logicalclocks.com>
2021-03-15 12:04:11 +0100

    update

--- hopsworks-installer.sh
+++ hopsworks-installer.sh
@@ -42,7 +42,7 @@
 ###################################################################################################
 
 HOPSWORKS_REPO=logicalclocks/ndb-chef
-HOPSWORKS_BRANCH=rondb
+HOPSWORKS_BRANCH=rondb_v2
 CLUSTER_DEFINITION_BRANCH=https://raw.githubusercontent.com/logicalclocks/karamel-chef/$HOPSWORKS_BRANCH
 KARAMEL_VERSION=0.6
 
