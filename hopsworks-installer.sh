91966a717705e137546c037e566aca053136890a
Jim Dowling <jim@logicalclocks.com>
2022-04-14 20:35:22 +0200

    opensearch_fixes

--- hopsworks-installer.sh
+++ hopsworks-installer.sh
@@ -28,7 +28,7 @@
 ###################################################################################################
 
 HOPSWORKS_REPO=logicalclocks/hopsworks-chef
-HOPSWORKS_BRANCH=master
+HOPSWORKS_BRANCH=opensearch_fixes
 CLUSTER_DEFINITION_BRANCH=https://raw.githubusercontent.com/logicalclocks/karamel-chef/$HOPSWORKS_BRANCH
 KARAMEL_VERSION=0.6
 ENTERPRISE_DOWNLOAD_URL=https://nexus.hops.works/repository
