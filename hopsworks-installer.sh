5c3e658dfabffd90c20610021afada403f3800ef
Jim Dowling <jim@logicalclocks.com>
2023-01-15 07:47:14 +0100

    fly duck, fly

--- hopsworks-installer.sh
+++ hopsworks-installer.sh
@@ -28,9 +28,9 @@
 ###################################################################################################
 
 HOPSWORKS_REPO=logicalclocks/hopsworks-chef
-HOPSWORKS_BRANCH=master
+HOPSWORKS_BRANCH=flyingduck
 CLUSTER_DEFINITION_BRANCH=https://raw.githubusercontent.com/logicalclocks/karamel-chef/$HOPSWORKS_BRANCH
-KARAMEL_VERSION=0.7
+KARAMEL_VERSION=0.8
 ENTERPRISE_DOWNLOAD_URL=https://nexus.hops.works/repository
 
 INSTALL_ACTION=
