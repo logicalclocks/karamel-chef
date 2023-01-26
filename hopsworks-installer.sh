63b2e8793d9a7301fa358fac486ca1647132d427
Jim Dowling <jim@logicalclocks.com>
2023-01-26 08:22:57 +0100

    Add flying duck Vagrantfile

--- hopsworks-installer.sh
+++ hopsworks-installer.sh
@@ -27,9 +27,9 @@
 #                                                                                                 #
 ###################################################################################################
 
-HOPSWORKS_REPO=jimdowling/hopsworks-chef
+HOPSWORKS_REPO=logicalclocks/hopsworks-chef
 HOPSWORKS_BRANCH=master
-CLUSTER_DEFINITION_BRANCH=https://raw.githubusercontent.com/logicalclocks/karamel-chef/flyingduck
+CLUSTER_DEFINITION_BRANCH=https://raw.githubusercontent.com/logicalclocks/karamel-chef/$HOPSWORKS_BRANCH
 KARAMEL_VERSION=0.7
 ENTERPRISE_DOWNLOAD_URL=https://nexus.hops.works/repository
 
