6253b42f773d720e3e355c154a5ecb5aae1d71b9
Jim Dowling <jim@logicalclocks.com>
2023-12-13 10:16:24 +0100

    update

--- hopsworks-installer.sh
+++ hopsworks-installer.sh
@@ -27,8 +27,8 @@
 #                                                                                                 #
 ###################################################################################################
 
-HOPSWORKS_REPO=logicalclocks/hopsworks-chef
-HOPSWORKS_BRANCH=master
+HOPSWORKS_REPO=jim_dowling/hopsworks-chef
+HOPSWORKS_BRANCH=k8s_upgrade
 CLUSTER_DEFINITION_BRANCH=https://raw.githubusercontent.com/logicalclocks/karamel-chef/$HOPSWORKS_BRANCH
 KARAMEL_VERSION=0.10-SNAPSHOT
 ENTERPRISE_DOWNLOAD_URL=https://nexus.hops.works/repository
